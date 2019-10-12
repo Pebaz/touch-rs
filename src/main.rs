8dec71cfe9dc2253af6cfe06ff40b216ef3e747c
Pebaz <pebazium@gmail.com>
2019-10-12 17:13:49 -0400

    Fixed folders always being created regardless of input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::path::Path;
 use std::fs::{File, create_dir_all};
 
 fn main() {
@@ -6,9 +7,23 @@ fn main() {
         println!("touch - Usage: touch <filename>");
     } else {
         let mut args = env::args().collect::<Vec<String>>();
+
+        // Remove executable name
         args.remove(0);
+
         for arg in args {
-            create_dir_all(arg.clone()).unwrap();
+            let parent = Path::new(&arg).parent().unwrap();
+
+            // Path is CWD
+            if parent.to_str().unwrap() == String::from("") {
+                File::create(arg).unwrap();
+            }
+
+            // Path is a folder path
+            else if !parent.exists() {
+                create_dir_all(parent).unwrap();
+                File::create(arg).unwrap();
+            }
         }
     }
 }
